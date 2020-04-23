392db5b9b0d7a5d034610d3b724950855262fcc3
Logan Garay <garay.logan@gmail.com>
2020-04-23 15:23:27 -0600

    routes set up and working

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,11 +1,44 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { ClientsComponent } from './components/clients/clients.component';
+import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { ClientFormComponent } from './components/client-form/client-form.component';
+import { EditClientComponent } from './components/edit-client/edit-client.component';
+import { ClientDetailsComponent } from './components/client-details/client-details.component';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { SettingsComponent } from './components/settings/settings.component';
+import { RouteNotFoundComponent } from './components/route-not-found/route-not-found.component';
 
+export const routingComponents = [
+  NavbarComponent,
+  DashboardComponent,
+  ClientDetailsComponent,
+  ClientFormComponent,
+  ClientsComponent,
+  SidebarComponent,
+  EditClientComponent,
+  LoginComponent,
+  RegisterComponent,
+  SettingsComponent,
+  RouteNotFoundComponent,
+];
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: DashboardComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'client/add', component: ClientFormComponent },
+  { path: 'client/edit/:id', component: EditClientComponent },
+  { path: 'client/:id', component: ClientDetailsComponent },
+  { path: 'settings', component: SettingsComponent },
+  { path: '**', component: RouteNotFoundComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,40 +1,13 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
+import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { NavbarComponent } from './components/navbar/navbar.component';
-import { DashboardComponent } from './components/dashboard/dashboard.component';
-import { ClientsComponent } from './components/clients/clients.component';
-import { SidebarComponent } from './components/sidebar/sidebar.component';
-import { ClientFormComponent } from './components/client-form/client-form.component';
-import { EditClientComponent } from './components/edit-client/edit-client.component';
-import { ClientDetailsComponent } from './components/client-details/client-details.component';
-import { LoginComponent } from './components/login/login.component';
-import { RegisterComponent } from './components/register/register.component';
-import { SettingsComponent } from './components/settings/settings.component';
-import { RouteNotFoundComponent } from './components/route-not-found/route-not-found.component';
 
 @NgModule({
-  declarations: [
-    AppComponent,
-    NavbarComponent,
-    DashboardComponent,
-    ClientsComponent,
-    SidebarComponent,
-    ClientFormComponent,
-    EditClientComponent,
-    ClientDetailsComponent,
-    LoginComponent,
-    RegisterComponent,
-    SettingsComponent,
-    RouteNotFoundComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, routingComponents],
+  imports: [BrowserModule, AppRoutingModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
