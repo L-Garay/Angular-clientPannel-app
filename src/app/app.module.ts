40a4d529e207450a933a79be774b437c022a614c
Logan Garay <garay.logan@gmail.com>
2020-04-23 15:40:04 -0600

    added a bunch of fire stuff

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,23 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { environment } from '../environments/environment';
+import { AngularFireModule } from '@angular/fire';
+import { AngularFireAnalyticsModule } from '@angular/fire/analytics';
+import { AngularFirestoreModule } from '@angular/fire/firestore';
+import { AngularFireAuthModule } from '@angular/fire/auth';
 
 @NgModule({
   declarations: [AppComponent, routingComponents],
-  imports: [BrowserModule, AppRoutingModule],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule,
+    AngularFireAuthModule,
+    AngularFireAnalyticsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,13 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: 'AIzaSyB1AwdeSa8H44B1epj91LSMQL6lzRnmcaI',
+    authDomain: 'angularpractice-9f882.firebaseapp.com',
+    databaseURL: 'https://angularpractice-9f882.firebaseio.com',
+    projectId: 'angularpractice-9f882',
+    storageBucket: 'angularpractice-9f882.appspot.com',
+    messagingSenderId: '610658029592',
+    appId: '1:610658029592:web:f604c6afcd8d672f3eb36d',
+    measurementId: 'G-4Q2YDNHNKM',
+  },
 };
