685e1d6841e16c5c1a34a66ecc1ea734063b70f2
Logan Garay <garay.logan@gmail.com>
2020-04-23 16:18:46 -0600

    can fetch clients from db, set in clients comp

--- src/app/components/clients/clients.component.ts
+++ src/app/components/clients/clients.component.ts
@@ -1,15 +1,21 @@
 import { Component, OnInit } from '@angular/core';
+import { ClientService } from '../../services/client.service';
+import { IClient } from '../../interfaces/Client';
 
 @Component({
   selector: 'app-clients',
   templateUrl: './clients.component.html',
-  styleUrls: ['./clients.component.css']
+  styleUrls: ['./clients.component.css'],
 })
 export class ClientsComponent implements OnInit {
+  clients: IClient[];
 
-  constructor() { }
+  constructor(private _clientService: ClientService) {}
 
   ngOnInit(): void {
+    this._clientService.getClients().subscribe((clients) => {
+      console.log(clients);
+      this.clients = clients;
+    });
   }
-
 }
