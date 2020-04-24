f06be7311faf00eb35cc7aaa32c4aeb2c378d74f
Logan Garay <garay.logan@gmail.com>
2020-04-23 21:25:15 -0600

    created form to add new client

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { environment } from '../environments/environment';
 import { AngularFireModule } from '@angular/fire';
@@ -18,6 +19,7 @@ import { ClientService } from './services/client.service';
     AngularFirestoreModule,
     AngularFireAuthModule,
     AngularFireAnalyticsModule,
+    FormsModule,
   ],
   providers: [ClientService],
   bootstrap: [AppComponent],
--- src/app/components/client-form/client-form.component.ts
+++ src/app/components/client-form/client-form.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { IClient } from '../../interfaces/Client';
+import { ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-client-form',
   templateUrl: './client-form.component.html',
-  styleUrls: ['./client-form.component.css']
+  styleUrls: ['./client-form.component.css'],
 })
 export class ClientFormComponent implements OnInit {
+  @ViewChild('clientForm') form: any;
+  client: IClient = {
+    firstName: '',
+    lastName: '',
+    email: '',
+    phone: '',
+    balance: 0,
+  };
+  disableBalanceOnAdd: boolean = true;
+  constructor() {}
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  ngOnInit(): void {}
 }
